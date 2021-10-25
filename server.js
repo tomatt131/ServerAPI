cad9749a8c8ee3a886f89e0b6a0d365220235d4a
EarnKCS <90103214+tomatt131@users.noreply.github.com>
2021-10-25 18:21:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', function(req, res) {
 
 app.post('/test', async function(req, res) {
     console.log(req.body);
-    console.log(req.Signature);
+    var requestData = JSON.parse(req.body);
     res.send("HELLO");
 })
 
