73584d4fe3227b49bd93a1e7b90e8123c187bf7a
EarnKCS <90103214+tomatt131@users.noreply.github.com>
2021-10-25 20:31:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -48,15 +48,16 @@ app.get('/', function(req, res) {
     res.send("<style>html {background: black;}</style><h1 style='font-size:60px;color:white;margin:auto;display:block;width:max-content;height:max-content;margin-top:40%;margin-bottom:50%;font-family:Roboto,serif;'>Server</h1>"); //Display the response on the website
 });
 
+app.use(bodyParser.json())
 app.post('/test', async function(req, res) {
     console.log(req.body);
+    console.log(JSON.parse(req.body));
     console.log(req.body.Signature);
     res.send("HELLO");
 })
 
-
+app.use(bodyParser.json())
 app.post('/auth', async function(req, res) {
-    console.log(req);
     console.log(req.body);
     let response = await checkSignatureValidity(req.body.Signer, req.body.SignatureHash);
     res.send({"Vaild":response});
