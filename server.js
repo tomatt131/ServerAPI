a1ac66c28aea3382380c7dc83ff45d4ce4d62ac5
EarnKCS <90103214+tomatt131@users.noreply.github.com>
2021-10-25 19:31:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://jsfiddle.net');
+    res.setHeader('Access-Control-Allow-Origin', 'https://fiddle.jshell.net, https://jsfiddle.net,');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
